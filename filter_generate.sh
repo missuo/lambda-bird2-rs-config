ca0d101e9784f5bcd319530890c454b1a3311a30
SteveYi <jackcooku@steveyi.net>
2022-01-04 03:34:20 +0800

    Add bird configure and update the filter script

--- filter_generate.sh
+++ filter_generate.sh
@@ -6,6 +6,7 @@ member_list="https://lambda-ix.net/members.json"
 # Generate Filter
 while [ $i -lt $total ]
 do
+
 	# ASN
 	ASN=$(curl https://lambda-ix.net/members.json | jq '.['$i'].asn')
 	ASN=${ASN//'"'/}
@@ -16,17 +17,35 @@ do
 
 	# Print
 	echo $ASN
+	AS_SET=${AS_SET#*::}
+
+	if [ ! $AS_SET ];
+	then
+		AS_SET="AS"$ASN
+	fi
 	echo $AS_SET
-	i=`expr $i + 1`
 
 	# Generate filter
-	filter_v4=$(bgpq3 -4l "AS"$ASN"_v4" $AS_SET -R 24 -b)
-	filter_v6=$(bgpq3 -6l "AS"$ASN_"v6" $AS_SET -R 48 -b)
+	filter_v4=$(bgpq3 -4l "define AS"$ASN"_v4" $AS_SET -R 24 -b)
+	filter_v6=$(bgpq3 -6l "define AS"$ASN"_v6" $AS_SET -R 48 -b)
 
-cat << EOF > filter/"AS"$ASN"_v4"
+	if [[ ! $filter_v4 ]];
+	then
+		filter_v4="define AS"$ASN"_v4 = [ 149.112.26.0/24 ];"
+	fi
+
+	if [[ ! $filter_v6 ]];
+	then
+		filter_v6="define AS"$ASN"_v6 = [ 2a0f:607:1070::/48 ];"
+	fi
+
+cat << EOF > /etc/bird/filters/"AS"$ASN"_v4"
 $filter_v4
 EOF
 
-	# echo $filter_v4
-	# echo $filter_v6
+cat << EOF > /etc/bird/filters/"AS"$ASN"_v6"
+$filter_v6
+EOF
+
+	i=`expr $i + 1`
 done
